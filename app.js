43ddab8dc656da287c6b2de837125e6cba9382b2
Alexeyenko Artyom <67514503+mkdir-s@users.noreply.github.com>
2022-08-18 21:04:16 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+gsap.to('.selector', {
+    x: 100,
+    y: 100,
+    background: 'orange',
+    duration: 1,
+    delay: .5,
+    repeat: 5,
+    repeatDelay: 1,
+    stagger: .1,
+    ease: 'power2.inOut',
+})
